3fe6b5d809eaebfc044966db8ec5f49876fa1fc0
Vladyslav56 <masliukov02@gmail.com>
2024-12-01 17:46:24 +0100

    Server upd13

--- server.js
+++ server.js
@@ -42,39 +42,3 @@ app.post("/search", async (req, res) => {
 app.listen(3000, () => {
 	console.log("Server running on port 3000")
 })
-
-// module.exports = async (res, req) => {
-// 	if (req.method === "POST") {
-// 		const { keyword } = req.body
-// 		if (!keyword) {
-// 			return res.status(400).json({ error: "Keyword is required" })
-// 		}
-// 		try {
-// 			const response = await axios.get(
-// 				`https://www.google.com/search?q=${encodeURIComponent(keyword)}&hl=en`,
-// 				{
-// 					timeout: 5000,
-// 					headers: {
-// 						"User-Agent":
-// 							"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-// 					},
-// 				}
-// 			)
-
-// 			const $ = cheerio.load(response.data)
-
-// 			const results = []
-// 			$(".yuRUbf").each((i, el) => {
-// 				const title = $(el).find("h3").text()
-// 				const link = $(el).find("a").attr("href")
-
-// 				results.push({ title, link })
-// 			})
-
-// 			res.json(results)
-// 		} catch (error) {
-// 			console.error(error)
-// 			res.status(500).json({ error: "Keyword is required" })
-// 		}
-// 	}
-// }
