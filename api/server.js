df79fe2b702b22408a8e3136397ad7e0e69e74e0
Vladyslav56 <masliukov02@gmail.com>
2024-12-01 15:30:50 +0100

    Server upd10

---
+++ api/server.js
@@ -0,0 +1,76 @@
+const express = require("express")
+const axios = require("axios")
+const cheerio = require("cheerio")
+
+const app = express()
+app.use(express.json())
+app.use(express.static("public"))
+
+app.post("api/search", async (req, res) => {
+	const { keyword } = req.body
+	if (!keyword) {
+		return res.status(400).json({ error: "Keyword is required" })
+	}
+	try {
+		const response = await axios.get(
+			`https://www.google.com/search?q=${encodeURIComponent(keyword)}&hl=en`,
+			{
+				headers: {
+					"User-Agent":
+						"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+				},
+			}
+		)
+
+		const $ = cheerio.load(response.data)
+
+		const results = []
+		$(".yuRUbf").each((i, el) => {
+			const title = $(el).find("h3").text()
+			const link = $(el).find("a").attr("href")
+
+			results.push({ title, link })
+		})
+
+		res.json(results)
+	} catch (error) {
+		console.error(error)
+		res.status(500).json({ error: "Keyword is required" })
+	}
+})
+
+// module.exports = async (res, req) => {
+// 	if (req.method === "POST") {
+// 		const { keyword } = req.body
+// 		if (!keyword) {
+// 			return res.status(400).json({ error: "Keyword is required" })
+// 		}
+// 		try {
+// 			const response = await axios.get(
+// 				`https://www.google.com/search?q=${encodeURIComponent(keyword)}&hl=en`,
+// 				{
+// 					timeout: 5000,
+// 					headers: {
+// 						"User-Agent":
+// 							"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+// 					},
+// 				}
+// 			)
+
+// 			const $ = cheerio.load(response.data)
+
+// 			const results = []
+// 			$(".yuRUbf").each((i, el) => {
+// 				const title = $(el).find("h3").text()
+// 				const link = $(el).find("a").attr("href")
+
+// 				results.push({ title, link })
+// 			})
+
+// 			res.json(results)
+// 		} catch (error) {
+// 			console.error(error)
+// 			res.status(500).json({ error: "Keyword is required" })
+// 		}
+// 	}
+// }
